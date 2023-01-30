a270efcc97d5dc57398fc7e8170ad8d94af567c0
Kagiso Modika <kagisomodika@yahoo.com>
2023-01-30 13:54:54 +0200

    Fourth day. First session.

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Asake",
+    "title": "Mr Money With The Vibe",
+    "release_year": 2022,
+    "formats": [
+      "Digital",
+      "LP"
+    ],
+    "gold": true
+  }
+];
