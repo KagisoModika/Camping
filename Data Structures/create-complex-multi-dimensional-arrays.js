9ac7d21c300e66364c1d3c09446d0831dd0b0a8f
Kagiso Modika <kagisomodika@yahoo.com>
2023-02-03 12:05:14 +0200

    Seventh day. Data structures 75% completion.

---
+++ Data Structures/create-complex-multi-dimensional-arrays.js
@@ -0,0 +1,9 @@
+let myNestedArray = [
+  // Only change code below this line
+  ['unshift', false, 1, 2, 3, ['deep'], 'complex', 'nested'],
+  ['loop', 'shift', [["deeper"]], 6, 7, 1000, 'method'],
+  ['concat', false, true, 'spread', 'array'],
+  ['mutate', [1, true], 1327.98, 'splice', 'slice', 'push'],
+  [['Hello', [5.2, ['deepest']]], 'iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+  // Only change code above this line
+];
